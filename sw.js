a916497f8f0677d5df2a154dc1799ede42140639
theaidran <134731756+theaidran@users.noreply.github.com>
2025-01-19 21:17:19 +0100

    v.1.6.7
    PWA

---
+++ sw.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'ela-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/reps.html',
+  '/styles.css',
+  '/reps.css',
+  '/script.js',
+  '/reps.js',
+  '/stats.js',
+  '/translations.js',
+  '/algorithms.js',
+  '/synchronization.js',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+  '/screenshots/desktop.svg',
+  '/screenshots/mobile.svg'
+];
+
+// Instalacja Service Workera
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Obsługa żądań
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Aktualizacja Service Workera
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+}); 
